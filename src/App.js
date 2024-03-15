9d4f42bc33c463ea38764136ebd365f6a86e17e7
Santananda <chaudharysantanand@gmail.com>
2024-03-15 13:54:43 +0545

    Headers Change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import RootLayout from './components/RootLayout';
 import HomePage from './pages/HomePage';
-import DetailPage from './pages/DetailPage';
 import ContactPage from './pages/ContactPage';
 
 const App = () => {
@@ -13,7 +12,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={<RootLayout />}>
           <Route index  element={<HomePage />} />
-          <Route path='detail/:id'  element={<DetailPage />} />
           <Route path='contact' element={<ContactPage />} />
            <Route path='about' element={<About />} />
            <Route path='*' element={<NotFound />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const navMenu = [
+    {path: 'about', name : 'About'},
+    {path : 'contact', name:'Contact'},
+    {path :'user', name:'User'}
+  ];   
+
+
   return (
     <div className='bg-green-200 text-white flex justify-between  
 px-4 py-2  items-baseline'>
-  <h1 className='text-3xl'> Logo</h1>
-  <nav className='space-x-5 '>
-    <NavLink to="about" className={(n)=>{
-      const { isActive} = n;
-      return isActive ? 'text-pink-900':'text-white'
-    }}> About </NavLink>
-    <NavLink to="contact">Contact</NavLink>
-    <NavLink to="user "> USer</NavLink>
+ 
+
+  <div className='flex flex-col space-y-2 '>
+    <h1 className='text-4xl font-bold' > Demo</h1>
+     {show && <div className=' sm:flex  hidden flex-col'>  {navMenu.map((nav, i)=>{
+return <NavLink key={i} to={nav.path} 
+className={(n)=>{
+  const {isActive} = n;
+  return isActive ? 'text-pink-400':'text-white'
+}}>
+  {nav.name}
+</NavLink>
+ })}</div>}
+  </div>
+  <button onClick={()=>setShow(!show)} className='hidden sm:flex'> {show ? <i className="fa-solid fa-square-xmark fa-lg"></i> : <i className="fa-solid fa-bars  fa-lg"></i>} </button>
+
+
+  <nav className='space-x-5 sm:hidden'>
+    {navMenu.map((nav, i)=>{
+      return <NavLink key={i} to={nav.path} 
+      className={(n)=>{
+        const {isActive} = n;
+        return isActive ? 'text-pink-400':'text-white'
+
+      }}>
+        {nav.name}
+      </NavLink>
+
+    })}
+
   </nav>
 </div>
   )
